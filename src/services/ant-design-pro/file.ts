85d6276fb26a82b926eaca19f2c0672b7b4cba8b
liangming <liangming1@lightinthebox.com>
2023-03-23 09:54:07 +0800

    feat: 查询URL地址更新

--- src/services/ant-design-pro/file.ts
+++ src/services/ant-design-pro/file.ts
@@ -1,8 +1,9 @@
 import { request } from 'umi';
+import { HOST } from '..';
 
 /** 获取列表数据 */
 export const getList = async (current?: number, pageSize?: number) => {
-  return request<any>('/api/file/list', {
+  return request<any>(HOST + '/api/file/list', {
     method: 'GET',
     params: {
       ...{ current, pageSize },
